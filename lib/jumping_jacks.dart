526a208671c05997621cce3fed149ac9df3223c5
Myron <myronmax_seelen@live.nl>
2024-09-23 23:34:42 +0200

    Outcomment Import

--- lib/jumping_jacks.dart
+++ lib/jumping_jacks.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_poc_gyroscope/pages/home_page.dart';
+//import 'package:flutter_poc_gyroscope/pages/home_page.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
 class JumpingJackCheck extends StatefulWidget {
