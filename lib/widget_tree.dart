65f13a9b2dd51a6e59ca19e3ba361f830bfaebe1
Myron <myronmax_seelen@live.nl>
2024-10-01 08:53:00 +0200

    small fixes

--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -4,7 +4,7 @@ import 'package:flutter_poc_gyroscope/pages/login_register_page.dart';
 import 'package:flutter/material.dart';
 
 class WidgetTree extends StatefulWidget {
-  const WidgetTree({ Key? key }) : super(key: key);
+  const WidgetTree({ super.key });
 
   @override
   State<WidgetTree> createState() => _WidgetTreeState();
@@ -18,7 +18,7 @@ class _WidgetTreeState extends State<WidgetTree> {
       stream: Auth().authStateChanges,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return HomePage();          
+          return const HomePage();          
         } else {
           return const LoginPage();
         }
